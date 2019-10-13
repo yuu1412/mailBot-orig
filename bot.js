568ee2569644b1bbc8440ae78dc98683f68ed3d8
yuu1412 <56495323+yuu1412@users.noreply.github.com>
2019-10-13 02:12:55 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,90 +23,51 @@ client.on('message', message => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
 	
-	switch (command){
-		case "blah":
-			message.channel.send('Meh.');
-			break;
-		case "foo":
-			message.channel.send('bar!');
-			break;
-		case "help":
-			message.channel.send('Instead of an actual help file, you should go to \<https://toyhou.se/1787487\> for the time being.');
-			break;
-		case "ping":
-			message.reply('pong');
-			break;
-		case "quirkfactor":
-			let numA = Math.floor(Math.random()*quirkList.length);
-			let numB = Math.floor(Math.random()*quirkList.length);
-			message.reply('you have a ' + quirkList[numA] + ' or a ' +  quirkList[numB] + ' quirk!');
-			break;
-		case "quirkpls":
-		case "quirkplz":
-			let num = Math.floor(Math.random()*quirkList.length);
-			let chosenQuirk = quirkList[num];
-			message.reply('you have a ' + chosenQuirk + ' quirk!');
-			break;
-		case "sauce":
-		case "saucepls":
-		case "source":
-			message.channel.send('If you don\'t know where the image came from, please try a reverse image search like Google, TinEye, ImgOps, or SauceNao!',{files:       ['https://file.toyhou.se/images/6364892_plnF1TceCBHi3Bg.png']});}
-			break;
-		case "say":
-			let text = args.join(" ");
-			message.delete();
-			message.channel.send(text);
-			break;
-		default:
-			message.reply('you failed. That wasn\'t valid, dumbass.');
-			
-	}
-	
-			/*
 	if (command === "ping") {message.reply('pong');}
-	if (command === "blah") {message.channel.send('Meh.');}
-	if (command === "foo") {message.channel.send('bar!');}
-	if (command === "help") {message.channel.send('Instead of an actual help file, you should go to \<https://toyhou.se/1787487\> for the time being.');}
+	else if (command === "blah") {message.channel.send('Meh.');}
+	else if (command === "foo") {message.channel.send('bar!');}
+	else if (command === "help") {message.channel.send('Instead of an actual help file, you should go to \<https://toyhou.se/1787487\> for the time being.');}
 	
-	if (command === "quirkfactor"){
+	else if (command === "quirkfactor"){
 		let numA = Math.floor(Math.random()*quirkList.length);
 		let numB = Math.floor(Math.random()*quirkList.length);
 		message.reply('you have a ' + quirkList[numA] + ' or a ' +  quirkList[numB] + ' quirk!');
 	}
-	if (command === "quirkpls" || command == "quirkplz"){
+	else if (command === "quirkpls" || command == "quirkplz"){
 		let num = Math.floor(Math.random()*quirkList.length);
 		let chosenQuirk = quirkList[num];
 		message.reply('you have a ' + chosenQuirk + ' quirk!');
 	}
-	if (command === "sauce") {message.channel.send('If you don\'t know where the image came from, please try a reverse image search like Google, TinEye, ImgOps, or SauceNao!',{files:       ['https://file.toyhou.se/images/6364892_plnF1TceCBHi3Bg.png']});}
+	else if (command === "sauce") {message.channel.send('If you don\'t know where the image came from, please try a reverse image search like Google, TinEye, ImgOps, or SauceNao!',{files:       ['https://file.toyhou.se/images/6364892_plnF1TceCBHi3Bg.png']});}
 	if(command === "say"){
 		let text = args.join(" ");
 		message.delete();
 		message.channel.send(text);
 	}
 	
-	if (command === "roll"){
+	else if (command === "roll"){
 		let text = args.join(" ");
 		//let diceNum = text.
 		//let diceType
 		message.channel.send('Eventually I will roll dice for you.');
 	}
-	if (command === "scripture"){
+	else if (command === "scripture"){
 		message.channel.send('Eventually I will quote scripture for you.');
 	}
-	if (command === "sprint"){
+	else if (command === "sprint"){
 		message.channel.send('Eventually I will run a sprint for you.');
 		//creates a 15min sprint that starts 3 min after a sprint is called unless a # is provided instead
 		//message.channel.send('We will start a sprint in 3 minutes.');
 		//setTimeout(sprintStart(), 180000);
 	}
-	if (command === "time"){
+	else if (command === "time"){
+		message.channel.send('When the sprint functionality is working, this will do something.');
 		//if a sprint is active, shows how much time is left
-		if (sprintActive == true){message.channel.send('There are ' + timer + ' minutes left to the sprint.');}
-		else {message.channel.send('There is no active sprint at this time.');}
+		//if (sprintActive == true){message.channel.send('There are ' + timer + ' minutes left to the sprint.');}
+		//else {message.channel.send('There is no active sprint at this time.');}
 	}
 	else{message.reply('you failed. That wasn\'t valid, dumbass.');}
-	*/
+	
 });
 
 // THIS  MUST  BE  THIS  WAY
