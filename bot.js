04d4234408ebd40bebe64eea366d1d8602afecb7
yuu1412 <56495323+yuu1412@users.noreply.github.com>
2019-10-12 23:02:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,26 @@ client.on('ready', () => {
 }); 
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-       message.reply('pong');
-       }
+    if (message.substring(0, 1) == 'mj!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+        
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            case 'help':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Please visit <https://toyhou.se/1787487> for documentation!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
 });
 
 // THIS  MUST  BE  THIS  WAY
