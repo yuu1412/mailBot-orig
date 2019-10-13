d6b6b09b636cb5a47ca74b40472a63ee5805dcba
yuu1412 <56495323+yuu1412@users.noreply.github.com>
2019-10-12 23:42:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "mj!";
 
 client.on('ready', () => {
     console.log('I am ready!');
 }); 
+
+let prefix = "mj!";
 
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
