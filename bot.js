140a7a815680299926eeaf708434dcebc1514710
yuu1412 <56495323+yuu1412@users.noreply.github.com>
2019-10-12 23:39:12 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = 'mj!';
+const prefix = "mj!";
 
 client.on('ready', () => {
     console.log('I am ready!');
