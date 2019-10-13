f857df3eabdce6de3afc0f9570ca0de1d032b21f
yuu1412 <56495323+yuu1412@users.noreply.github.com>
2019-10-13 00:07:29 -0400

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send('Instead of an actual help file, you should go to <https://toyhou.se/1787487> for the time being.').catch(console.error);
+}
